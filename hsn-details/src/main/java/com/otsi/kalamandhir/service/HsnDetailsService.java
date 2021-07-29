ae018d41cfb83ab9f3d68cc550e106292b91c9c1
VasaviYakkali <85737909+VasaviYakkali@users.noreply.github.com>
2021-07-29 14:21:16 +0530

    Update HsnDetailsService.java

--- hsn-details/src/main/java/com/otsi/kalamandhir/service/HsnDetailsService.java
+++ hsn-details/src/main/java/com/otsi/kalamandhir/service/HsnDetailsService.java
@@ -9,6 +9,7 @@ import java.util.List;
 import org.springframework.stereotype.Service;
 import com.otsi.kalamandhir.vo.EnumVo;
 import com.otsi.kalamandhir.vo.HsnDetailsVo;
+import com.otsi.kalamandhir.vo.TaxVo;
 
 /**
  * @author vasavi
@@ -24,5 +25,7 @@ public interface HsnDetailsService {
 	List<EnumVo> getEnums(String enumName);
 
 	String hsnDelete(long id);
+	
+	List<TaxVo> getTaxDetails();
 
 }
