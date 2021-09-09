ad1c4282b3971bbe1f564c1dd0bea6b5cd62ab7c
manideepTaninkiOtsi <manideep.taninki@otsi.co.in>
2021-09-09 11:37:52 +0530

    Release-1

--- hsn-details/src/main/java/com/otsi/retail/hsnDetails/HsnDetailsApplication.java
+++ hsn-details/src/main/java/com/otsi/retail/hsnDetails/HsnDetailsApplication.java
@@ -3,6 +3,8 @@ package com.otsi.retail.hsnDetails;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 @EnableDiscoveryClient
@@ -10,6 +12,12 @@ public class HsnDetailsApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(HsnDetailsApplication.class, args);
+	}
+	
+	@Bean
+	public RestTemplate restTemplate() {
+		return new RestTemplate();
+		
 	}
 
 }
