4d9469f8c76a1bfb6e48f00cae7e8ae542005305
vasavi.yakkali <vasavi.yakkali@otsi.co.in>
2021-09-16 10:45:36 +0530

    added gateway response and added logging

--- payment-gateway/src/main/java/com/otsi/retail/paymentgateway/controller/PaymentGatewayController.java
+++ payment-gateway/src/main/java/com/otsi/retail/paymentgateway/controller/PaymentGatewayController.java
@@ -1,16 +1,19 @@
 /*
- * controller for save,update,delete and getEnums
+ * controller for save
  */
 package com.otsi.retail.paymentgateway.controller;
 
 import java.util.Map;
-
-import com.otsi.retail.paymentgateway.service.PaymentGatewayService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import com.otsi.retail.paymentgateway.gatewayresponse.GateWayResponse;
+import com.otsi.retail.paymentgateway.service.PaymentGatewayService;
 
 /**
  * @author vasavi
@@ -20,20 +23,19 @@ import org.springframework.stereotype.Controller;
 @RequestMapping("/paymentgateway")
 public class PaymentGatewayController {
 
-    private Logger log = LoggerFactory.getLogger(PaymentGatewayController.class);
+	private Logger log = LoggerFactory.getLogger(PaymentGatewayController.class);
 
-    @Autowired
-    private PaymentGatewayService paymentGatwayService;
+	@Autowired
+	private PaymentGatewayService paymentGatwayService;
 
-    @PostMapping("/create_order")
-    @ResponseBody
-    public String createOrder(@RequestBody Map<String, Object> data) throws Exception
-    {
-        log.info("Inside the create order service controller");
-        System.out.println(data);
-        int amt=Integer.parseInt(data.get("amount").toString());
-        return paymentGatwayService.createOrder(amt);
-    }
+	@PostMapping("/create_order")
+	@ResponseBody
+	public GateWayResponse<?> createOrder(@RequestBody Map<String, Object> data) throws Exception {
+		log.info("Inside the create order service controller");
+		System.out.println(data);
+		int amt = Integer.parseInt(data.get("amount").toString());
+		return new GateWayResponse<String>(paymentGatwayService.createOrder(amt));
 
+	}
 
 }
