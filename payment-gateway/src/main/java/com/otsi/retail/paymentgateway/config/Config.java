85983a55c6c0eb0851786eb11ea1d36ea97ac0d0
Manikanta Guptha <narayana.chitturi@otsi.co.in>
2021-11-23 10:12:39 +0530

    Added RabbitMQ configuration

--- payment-gateway/src/main/java/com/otsi/retail/paymentgateway/config/Config.java
+++ payment-gateway/src/main/java/com/otsi/retail/paymentgateway/config/Config.java
@@ -3,7 +3,18 @@ package com.otsi.retail.paymentgateway.config;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
+import org.springframework.amqp.core.AmqpTemplate;
+import org.springframework.amqp.core.Binding;
+import org.springframework.amqp.core.BindingBuilder;
+import org.springframework.amqp.core.DirectExchange;
+import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.amqp.support.converter.MessageConverter;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
@@ -11,11 +22,48 @@ import org.springframework.context.annotation.Configuration;
 @AllArgsConstructor
 @NoArgsConstructor
 public class Config {
-    @Value("${RazoryPay.key_Id}")
-    private String Key;
-    @Value("${RazorPay.key_Secret}")
-    private String secert;
+	@Value("${RazoryPay.key_Id}")
+	private String Key;
+	
+	@Value("${RazorPay.key_Secret}")
+	private String secert;
+	
+	@Value("${newsale_queue}")
+	private String newSaleQueue;
+	
+	@Value("${newsale_exchange}")
+	private String newSaleExchange;
+	
+	@Value("${payment_newsale_rk}")
+	private String paymentNewsaleRK;
+	
+	@Bean
+	public Queue queue() {
+		return new Queue(newSaleQueue);
+	}
 
-    @Value("${paymenyUpdateNewsale}")
-    private String restCallForNewsalePayments;
+	@Bean
+	public DirectExchange directExchange() {
+		return new DirectExchange(newSaleExchange);
+	}
+
+	@Bean
+	public Binding binding(Queue queue, DirectExchange directExchange) {
+
+		return BindingBuilder.bind(queue).to(directExchange).with(paymentNewsaleRK);
+	}
+
+	@Bean
+	public MessageConverter messageConverter() {
+		return new Jackson2JsonMessageConverter();
+	}
+
+	@Bean
+	public AmqpTemplate template(ConnectionFactory factory) {
+
+		RabbitTemplate template = new RabbitTemplate(factory);
+		template.setMessageConverter(messageConverter());
+		return template;
+
+	}
 }
--- payment-gateway/src/main/java/com/otsi/retail/paymentgateway/service/RazorPayImpl.java
+++ payment-gateway/src/main/java/com/otsi/retail/paymentgateway/service/RazorPayImpl.java
@@ -1,28 +1,21 @@
 package com.otsi.retail.paymentgateway.service;
 
-import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
 
-import com.otsi.retail.paymentgateway.gatewayresponse.GateWayResponse;
 import org.json.JSONObject;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.otsi.retail.paymentgateway.config.Config;
 import com.otsi.retail.paymentgateway.vo.PaymentDetailsVo;
+import com.otsi.retail.paymentgateway.vo.RazorPayResponse;
 import com.razorpay.Order;
 import com.razorpay.Payment;
 import com.razorpay.RazorpayClient;
-import org.springframework.web.client.RestTemplate;
 
 @Component
 public class RazorPayImpl implements PaymentGatewayService {
@@ -33,10 +26,9 @@ public class RazorPayImpl implements PaymentGatewayService {
 	Config config;
 
 	@Autowired
-	private RestTemplate template;
+	private RabbitTemplate rabbitTemplate;
 
 	@Override
-	// @SuppressWarnings("unchecked")
 	public Order createOrder(int amount, String newsaleOrderNumber) throws Exception {
 		log.debug("debugging createOrder():" + amount);
 		log.info("Inside the create method of service class");
@@ -51,30 +43,23 @@ public class RazorPayImpl implements PaymentGatewayService {
 		// creating new order
 
 		Order order = client.Orders.create(ob);
-		System.out.println(order);
+		log.info("order created succesfully:" + order.toString());
 
-		// KLModerid ,razor pay id-payment table rest call to new sale...
-		List<PaymentDetailsVo> listVo = new ArrayList<>();
-		
+		ObjectMapper objectMapper = new ObjectMapper();
+		RazorPayResponse razorPayResponse = objectMapper.readValue(order.toString(), RazorPayResponse.class);
 		PaymentDetailsVo vo = new PaymentDetailsVo();
+
 		vo.setNewsaleOrder(newsaleOrderNumber);
 		vo.setAmount(Long.valueOf(amount));
-		listVo.add(vo);
-		// HashMap<String,Object> result =
-		// new ObjectMapper().readValues(order,HashMap.class);
+		vo.setPayType("Card");
+		vo.setRazorPayId(razorPayResponse.getId());
 
-		HttpHeaders headers = new HttpHeaders();
-		headers.setContentType(MediaType.APPLICATION_JSON);
+		// Pass object to Rabbit queue
+		rabbitTemplate.convertAndSend(config.getNewSaleExchange(), config.getPaymentNewsaleRK(), vo);
+		log.info("order details queue send to newsale " + order.toString());
 
-		HttpEntity<List<String>> request = new HttpEntity(listVo, headers);
-
-		ResponseEntity<GateWayResponse> response = template.exchange(config.getRestCallForNewsalePayments(),
-				HttpMethod.POST, request, GateWayResponse.class);
-
-		String message = response.getBody().getMessage();
-
-		log.info("order created succesfully:" + order.toString());
 		return order;
+
 	}
 
 	@Override
